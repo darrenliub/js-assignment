4d395f3389ae859e4762b60275d31507a701e89d
darrenliub <darrenliu@pacificamerican.org>
2023-01-09 09:56:43 +0800

    add assignments

---
+++ lesson1.js
@@ -0,0 +1,21 @@
+const age = 20
+const year = 2025
+
+let favnum = 69
+favnum = 7
+favnum = 12
+
+const birthYear = 1990
+
+const student1 = 'Skyler'
+const student2 = 'Cindy'
+const student3 = 'Judy'
+const teacher = 'Mr.Kung'
+
+document.write("<p class = 'first-msg'>Hi " + student1 + "<br>")
+document.write(teacher + " is born in "+ birthYear + "<br>")
+
+document.write(`${teacher} is born in ${birthYear} and he teaches ${student1} Web Dev`)
+
+const friend = prompt ("What is your best friend's name?<br>")
+const main = prompt ("Who are you?<br>")
