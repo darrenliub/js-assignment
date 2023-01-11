d3df8ab401a5910575a3775c8d277fa89b6b09d9
darrenliub <darrenliu@pacificamerican.org>
2023-01-11 10:40:51 +0800

    done

--- variable-story/story.js
+++ variable-story/story.js
@@ -1,13 +1,9 @@
-// const main = prompt("What is your name?")
-// const minor = prompt("Who is your best friend?")
-// const place = prompt("What is your favorite country?")
-// const food = prompt("what is your favorite food?")
-// const weather = prompt("what is your favorite weather? (has to have y in at the end)")
-const main = 'Darren'
-const minor = 'Evan'
-const place = 'Japan'
-const food = 'nuggets'
-const weather = 'stormy'
+const main = prompt("What is your name?")
+const minor = prompt("Who is your best friend?")
+const place = prompt("What is your favorite country?")
+const food = prompt("what is your favorite food?")
+const weather = prompt("what is your favorite weather? (has to have y in at the end)")
+
 
 document.write ("<p class = 'text'><br>Once upon a time, " + main + " and his friend, " + minor + " is going on a trip to find the " + food + 
 ".<br><br>After years and years of traveling, they finaly find the " + food + " in the far away country, " + place + 
